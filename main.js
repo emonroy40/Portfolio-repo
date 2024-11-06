eaa5e3c639220be3a120787c0a1aabf0a1a42ff3
emonroy40 <emon.roy4037@gmail.com>
2024-11-07 01:01:16 +0600

    create new repo

---
+++ main.js
@@ -0,0 +1,9 @@
+var type = new Typed(".multiple_text", {
+    strings: ["Frontend Developer", "Web Designer", "Wordpress Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    typeDaly: 1000,
+    loop: true
+})
+
+//tab section
